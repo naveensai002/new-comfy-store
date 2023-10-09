b11568579091486c9c09d5b1fc42fd81ec7126cc
naveensai <sainaveen9000@gmail.com>
2023-10-09 20:50:56 +0530

    about page

--- comfy-store/src/pages/About.jsx
+++ comfy-store/src/pages/About.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
 const About = () => {
-  return <div className='text-green-500'>About</div>;
+  return (
+    <div>
+      <div className='flex mb-10 flex-row gap-6 items-center justify-center p-4 text-xl '>
+        <h1>We love</h1>
+        <span className='bg-rose-500 p-4 rounded-md text-xl transition'>
+          comfy store
+        </span>
+      </div>
+      <div className=' flex justify-center text-center tracking-tight leading-8 w-full max-w-[70vw] mx-auto text-lg xs:text-sm '>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui culpa
+        impedit amet eveniet facere quaerat placeat debitis id at ut eum dolor,
+        aspernatur voluptates reiciendis nulla suscipit illum illo iure ipsum
+        aliquam, asperiores ad hic? Enim repellat laboriosam commodi vel?
+      </div>
+    </div>
+  );
 };
 
 export default About;
